800407a86349d376f4a6e67163f4bc20b9d9ad2a
Karl Merkli <k.merkli@eslgaming.com>
2018-02-26 17:59:16 +0100

    Fixed not working

--- filter-statuses.js
+++ filter-statuses.js
@@ -1,11 +1,11 @@
 module.exports = (whitelist, blacklist) => {
     return variants => {
         if (whitelist && whitelist.length) {
-            return variants.filter(variant => whitelist.includes(component.status));
+            return variants.filter(variant => whitelist.includes(variant.status));
         }
 
         if (blacklist && blacklist.length) {
-            return variants.filter(variant => !blacklist.includes(component.status));
+            return variants.filter(variant => !blacklist.includes(variant.status));
         }
 
         return variants;
